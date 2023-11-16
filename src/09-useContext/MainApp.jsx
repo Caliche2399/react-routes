fd4d7da17c1e255853e23dc26272b73ce72296ae
Caliche2399 <carlosaguilarpaniagua@gmail.com>
2023-11-16 15:01:50 -0600

    .
    Se crea el nabvar de la aplicación

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,13 +1,16 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes} from "react-router-dom"
 import { HomePage } from "./HomePage"
 import { LoginPage } from "./LoginPage"
 import { AboutPage } from "./AboutPage"
+import { NavBar } from "./components/NavBar"
 
 export const MainApp =() => {
   return (
     <>
         <h1>MainApp</h1>
         <hr />
+
+        <NavBar/>
 
         <Routes>
             <Route path="/" element={<HomePage/>}/>
