192632cbcc3abe5f866d1e64e55b59eb8546bd97
Caliche2399 <carlosaguilarpaniagua@gmail.com>
2023-11-16 15:11:02 -0600

    Se agrega tabulación para lectura de código

--- src/09-useContext/components/NavBar.jsx
+++ src/09-useContext/components/NavBar.jsx
@@ -7,20 +7,20 @@ export const NavBar = () => {
             <Link className="navbar-brand" to = "/">useContext</Link>
 
             <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+                <ul className="navbar-nav">
 
-                <NavLink to="/" className={({isActive}) => `nav-link ${isActive ? 'active': ''}`}>
-                    Home
-                </NavLink>
+                    <NavLink to="/" className={({isActive}) => `nav-link ${isActive ? 'active': ''}`}>
+                        Home
+                    </NavLink>
 
-                <NavLink to="/about" className={({isActive}) => `nav-link ${isActive ? 'active': ''}`}>
-                    About
-                </NavLink>
-                
-                <NavLink to="/login" className={({isActive}) => `nav-link ${isActive ? 'active': ''}`}>
-                    Login
-                </NavLink>
-            </ul>
+                    <NavLink to="/about" className={({isActive}) => `nav-link ${isActive ? 'active': ''}`}>
+                        About
+                    </NavLink>
+                    
+                    <NavLink to="/login" className={({isActive}) => `nav-link ${isActive ? 'active': ''}`}>
+                        Login
+                    </NavLink>
+                </ul>
             </div>
         </div>
     </nav>
